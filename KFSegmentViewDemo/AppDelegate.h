7b8bfe5891b7293eac0cc42cf5a0886c6ca46479
ismilesky <fighter_kongfei@126.com>
2015-11-04 15:13:56 +0800

    Initial Commit

---
+++ KFSegmentViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KFSegmentViewDemo
+//
+//  Created by VS on 15/11/4.
+//  Copyright © 2015年 VS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
